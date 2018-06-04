cbd186a42010746a125b675743a08ae6de54332a
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-06-04 21:23:32 +0300

    Added loading of data from text file

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,4 +1,13 @@
 import argparse
+import os.path
+import sys
+
+
+def load_text_data(filepath):
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as file:
+        return file.read()
 
 
 def parse_command_line_arguments():
@@ -18,6 +27,17 @@ def main():
     command_line_arguments = parse_command_line_arguments()
 
     filename = command_line_arguments.filename
+
+    try:
+        text = load_text_data(filename)
+    except UnicodeDecodeError:
+        sys.exit('text file has invalid format')
+
+    if text is None:
+        sys.exit('file not found')
+
+    if not text:
+        sys.exit('file is empty')
 
 
 if __name__ == '__main__':
