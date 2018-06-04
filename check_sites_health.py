c92e069371251a3019330ea18e996dc49a719ee6
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-06-04 19:18:01 +0300

    Added main script function

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,6 @@
-def load_urls4check(path):
+def main():
     pass
 
-def is_server_respond_with_200(url):
-    pass
-
-def get_domain_expiration_date(domain_name):
-    pass
 
 if __name__ == '__main__':
-    pass
+    main()
