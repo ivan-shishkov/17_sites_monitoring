dc64fc340476010060f7677646b923491b94eb80
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-06-04 20:27:49 +0300

    Added parsing of command line arguments

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,5 +1,23 @@
+import argparse
+
+
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        'filename',
+        help='a text file containing URLs of checked sites',
+        type=str,
+    )
+    command_line_arguments = parser.parse_args()
+
+    return command_line_arguments
+
+
 def main():
-    pass
+    command_line_arguments = parse_command_line_arguments()
+
+    filename = command_line_arguments.filename
 
 
 if __name__ == '__main__':
